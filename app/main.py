d240c1cb70c48f65e3d3629d3ca949253aec0b17
kmkimhr <kmkimhr@gmail.com>
2026-06-24 23:38:47 +0900

    ADD: router 추가

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 
-from app.api.v1.health import router as health_router
+from app.api.v1.router import api_router
 
 app = FastAPI()
 
-app.include_router(health_router, prefix="/api/v1")
+app.include_router(api_router, prefix="/api/v1")
